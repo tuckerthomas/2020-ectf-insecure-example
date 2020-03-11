6ac627949d9fb4f5750c594c34e542bf1efc2c01
Justin Campbel <techdev5521@users.noreply.github.com>
2020-03-11 00:04:34 -0400

    Install pip3, pynacl and build BearSSL

--- vagrant/customizations.sh
+++ vagrant/customizations.sh
@@ -2,5 +2,12 @@
 # THIS MUST BE UPDATED TO WORK WITH YOUR DESIGN SO THAT ALL TOOLS ARE ON THE PATH
 export PATH="/ectf/tools:$PATH"
 echo "export PATH=/ectf/tools:$PATH" >> ~/.bashrc
+
+# Install Python 3 Package Manager
 sudo apt install -y python3-pip
-sudo pip3 install numpy
+
+# Install PyNacl Library
+sudo pip3 install pynacl
+
+# Built BearSSL Library
+cd /ectf/BearSSL && make CONF=Microblaze
